19e09c6e1df5fd08011a527d494ed4f4484c5b1e
klin2391 <lin2391@bu.edu>
2023-04-06 17:26:33 -0400

    Add game loop

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args){
-        // MonstersAndHeroes game = new MonstersAndHeroes();
-        // game.start();
-        int effectValue = 100;
-        effectValue += (effectValue * 0.1);
-        System.out.println(effectValue);
-        effectValue -= (effectValue * 1/11);
-        System.out.println(effectValue);
-        WorldLegends game = new WorldLegends(12);
-        // HeroTeam ht = new HeroTeam();
-        game.generateWorld();
-        game.showGlobalView();
+        MonstersAndHeroesLegends game = new MonstersAndHeroesLegends();
+        game.start();
+        // int effectValue = 100;
+        // effectValue += (effectValue * 0.1);
+        // System.out.println(effectValue);
+        // effectValue -= (effectValue * 1/11);
+        // System.out.println(effectValue);
+        // WorldLegends game = new WorldLegends(12);
+        // // HeroTeam ht = new HeroTeam();
+        // game.generateWorld();
+        // game.showGlobalView();
     }
 }
